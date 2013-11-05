47ea18e2452eaff81aacca18387f82a464b566ce
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-11-05 10:23:34 +0100

    Added `$rootScope.rbsCurrentPluginName`, containing the name of the current plugin (the plugin where the user is in the backoffice UI).

--- Admin/Assets/js/admin.js
+++ Admin/Assets/js/admin.js
@@ -1,40 +1,28 @@
 (function () {
 
+	"use strict";
+
 	var app = angular.module('RbsChange');
 
+	app.config(['$provide', function ($provide)
+	{
+		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
+		{
+			$delegate.model('Rbs_Plugins')
+				.route('Installed', 'Rbs/Plugins/Installed/', { 'templateUrl': 'Rbs/Plugins/Installed/list.twig'})
+				.route('Registered', 'Rbs/Plugins/Registered/', { 'templateUrl': 'Rbs/Plugins/Registered/list.twig'})
+				.route('New', 'Rbs/Plugins/New/', { 'templateUrl': 'Rbs/Plugins/New/list.twig'});
+			return $delegate;
+		}]);
+	}]);
 
 	app.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
-
-		// Plugins
-
 		. when(
 			'/Rbs/Plugins/',
 			{
 				redirectTo : '/Rbs/Plugins/Installed/'
 			})
-
-			. when(
-			'/Rbs/Plugins/Installed/',
-			{
-				templateUrl : 'Rbs/Plugins/Installed/list.twig',
-				reloadOnSearch : false
-			})
-
-			. when(
-			'/Rbs/Plugins/Registered/',
-			{
-				templateUrl : 'Rbs/Plugins/Registered/list.twig',
-				reloadOnSearch : false
-			})
-
-			. when(
-			'/Rbs/Plugins/New',
-			{
-				templateUrl : 'Rbs/Plugins/New/list.twig',
-				reloadOnSearch : false
-			})
-
 		;
 	}]);
 
