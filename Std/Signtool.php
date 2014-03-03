96a761caeb950fc69139dbdcd22ab592be29a6c3
Franck STAUFFER <franck.stauffer@rbs.fr>
2014-03-04 00:00:01 +0100

    PHP Code Template Update

--- Std/Signtool.php
+++ Std/Signtool.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: fstauffer
- * Date: 18/06/13
- * Time: 14:32
- * To change this template use File | Settings | File Templates.
+ * Copyright (C) 2014 Ready Business System
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 namespace Rbs\Plugins\Std;
 
 
